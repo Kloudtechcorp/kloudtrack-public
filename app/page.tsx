8bfc581aa9c53d0437a0795a1cd469a2c3b76c3d
Jam <102900718+Jaaammmm@users.noreply.github.com>
2024-10-02 17:04:47 +0800

    Weather Cards Template

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,12 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
+import CurrentWeather from "@/components/shared/currentWeather";
+import DailyCards from "@/components/shared/dailyCards";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen p-8 bg-gradient-to-b from-[#66CCFF] from-0% via-[#CCFFFF] via-75% to-[#FFFFFF] to-100% ">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Card className="p-3 px-5 flex gap-2 bg-transparent border-transparent">
-            <span className=""> Filler Icon</span>
-            <div>
-              <CardTitle>Umbrellas</CardTitle>
-              <div className="flex justify-center items-center gap-1">
-                <span className="border-green-500 border-4 rounded h-2 flex "></span>
-                <CardDescription>Card Description</CardDescription>
-              </div>
-            </div>
-          </Card>
-        </div>
+    <div className="flex p-8 bg-gradient-to-b from-[#66CCFF] from-0% via-[#CCFFFF] via-75% to-[#FFFFFF] to-100% min-h-svh">
+      <main className="flex flex-col gap-4 container mx-auto">
+        <CurrentWeather />
+        <DailyCards />
       </main>
     </div>
   );
---
+++ components/shared/dailySuggestions.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+interface DailySuggestionProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+  altText: string;
+  color: string;
+}
+
+const dailySuggestionsData: DailySuggestionProps[] = [
+  {
+    title: "Jogging",
+    description: "Highly suggested",
+    imageSrc: "assets/dailyIcons/jog.svg",
+    altText: "Jog",
+    color: "green-500",
+  },
+  {
+    title: "Cycling",
+    description: "Highly suggested",
+    imageSrc: "assets/dailyIcons/cycle.svg",
+    altText: "Cycle",
+    color: "green-500",
+  },
+  {
+    title: "Swimming",
+    description: "Highly suggested",
+    imageSrc: "assets/dailyIcons/swim.svg",
+    altText: "Swim",
+    color: "green-500",
+  },
+  {
+    title: "Camping",
+    description: "Highly suggested",
+    imageSrc: "assets/dailyIcons/camp.svg",
+    altText: "Camp",
+    color: "yellow-500",
+  },
+  {
+    title: "Sports",
+    description: "Safe",
+    imageSrc: "assets/dailyIcons/ball.svg",
+    altText: "Sports",
+    color: "green-500",
+  },
+];
+
+const DailySuggestions = () => {
+  return (
+    <div className="flex gap-2 px-1 flex-col w-full">
+      {dailySuggestionsData.map((card, index) => (
+        <Card
+          key={index}
+          className="p-3 px-4 flex gap-3 bg-[#FBFBFB] bg-opacity-50 border-transparent rounded-md w-72"
+        >
+          <div className="flex items-start">
+            <Image
+              src={card.imageSrc}
+              alt={card.altText}
+              width={40}
+              height={40}
+            />
+          </div>
+          <div className="flex flex-col justify-center pr-3">
+            <CardTitle>{card.title}</CardTitle>
+
+            <div className="flex justify-start items-center gap-1">
+              <span
+                className={`border-${card.color} border-4 rounded h-2 flex`}
+              ></span>
+              <CardDescription className="text-black">
+                {card.description}
+              </CardDescription>
+            </div>
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default DailySuggestions;
